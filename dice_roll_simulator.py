6c4d1f3bcf18954a20e6b02882c4759f3f325f98
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-21 16:12:10 +0000

    Final commit

--- dice_roll_simulator.py
+++ dice_roll_simulator.py
@@ -5,3 +5,61 @@
 """
 import random
 
+
+def roll_dice():
+    dice_drawing = {
+        1: (
+            "-----"
+            "|   |"
+            "| o |"
+            "|   |"
+            "-----"
+        ),
+        2: (
+            "-----"
+            "|o  |"
+            "|   |"
+            "|  o|"
+            "-----"
+        ),
+        3: (
+            "-----"
+            "|o  |"
+            "| o |"
+            "|  o|"
+            "-----"
+        ),
+        4: (
+            "-----"
+            "|o o|"
+            "|   |"
+            "|o o|"
+            "-----"
+        ),
+        5: (
+            "-----"
+            "|o o|"
+            "| o |"
+            "|o o|"
+            "-----"
+        ),
+        6: (
+            "-----"
+            "|o o|"
+            "|o o|"
+            "|o o|"
+            "-----"
+        )
+    }
+    option = input("Roll dice? (Y/N)")
+
+    while option.lower() == "y":
+        dice1 = random.randint(1, 6)
+        dice2 = random.randint(1, 6)
+
+        print(f"dice rolled {dice1} and {dice2}")
+        print(dice_drawing[dice1])
+        print(dice_drawing[dice2])
+        option = input('Roll dice? (Y/N): ')
+
+roll_dice()
